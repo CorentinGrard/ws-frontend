74c0651a3370b5a2e17ea84b44e4a9569bc06fa7
Corentin <corentin.grard@ibm.com>
2020-01-20 09:49:15 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; 2020</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      
+    </v-flex>
+    {{data}}
+    <!-- <v-data-table
+      :headers="headers"
+      :items="data"
+    ></v-data-table> -->
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      headers: [
+        { text: 'ID', value: 'id' },
+        { text: 'Company', value: 'company' },
+        { text: 'Place', value: 'place' },
+        { text: 'Price', value: 'price' },
+        { text: 'Date', value: 'date' },
+      ],
+      data: []
+    }
+  },
+  async created() {
+    let response = await this.$axios.get("localhost:5000/api/v1/vol");
+    this.data = response.data
+
+  },
+}
+</script>
